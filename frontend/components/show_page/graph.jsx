56b56954c89e11a34ca0be3549a22cffdb0b7bf5
Brennan <brennan.romance@gmail.com>
2020-03-06 16:17:51 -0500

    chart is functional for intraday prices on show page

--- frontend/components/show_page/graph.jsx
+++ frontend/components/show_page/graph.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts';
 
 class ShowPageGraph extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            data: '',
+        }
     }
 
     componentDidMount() {
@@ -22,18 +25,15 @@ class ShowPageGraph extends React.Component {
             return null;
         } else {
             data = this.props.dayPrices;
-            let top;
-            let bottom;
-            for(let i = 1; i < data.length; i++) {
-                
-            }
         }
 
         const renderLineChart = (
             <LineChart width={400} height={400} data={data}>
                 <Line type="monotone" dataKey="close" stroke="#8884d8" />
                 <CartesianGrid stroke="#ccc" />
-                <YAxis />
+                <YAxis domain={['dataMin', 'dataMax']}/>
+                <XAxis />
+                <Tooltip />
             </LineChart>
         );
         return (
