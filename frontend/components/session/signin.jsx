cf53bf45f890394a3628a585656be3de57a566a5
Brennan Romance <brennan.romance@gmail.com>
2020-04-23 10:38:40 -0400

    add more demo users

--- frontend/components/session/signin.jsx
+++ frontend/components/session/signin.jsx
@@ -28,7 +28,12 @@ class Signin extends React.Component {
     let c = { username: 'demo3', password: 'password' };
     let d = { username: 'demo4', password: 'password' };
     let e = { username: 'demo5', password: 'password' };
-    let array = [a, b, c, d, e];
+    let f = { username: 'demo6', password: 'password' };
+    let g = { username: 'demo7', password: 'password' };
+    let h = { username: 'demo8', password: 'password' };
+    let i = { username: 'demo9', password: 'password' };
+    let j = { username: 'demo10', password: 'password' };
+    let array = [a, b, c, d, e, f, g, h, i, j];
     let rand = array[Math.floor(Math.random() * array.length)];
     this.props.signin(rand)
     setTimeout(() => {
