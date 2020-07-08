d910cb2fd10a716f7c5406b6139b812d25682845
Brennan <brennan.romance@gmail.com>
2020-07-08 14:56:11 -0400

    make historical fetch util method dynamic

--- frontend/util/graph_api_util.js
+++ frontend/util/graph_api_util.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 export const fetchStockDay = (ticker) => (
     $.ajax({
         url: `https://financialmodelingprep.com/api/v3/historical-chart/5min/${ticker}?apikey=${window.stockapikey}`
@@ -11,9 +13,11 @@ export const fetchStockWeek = (ticker) => (
 )
 
 export const fetchstockHistorical = (ticker) => {
-    let d = new Date();
+    let date = new Date();
+    let today = moment(date).format('YYYY-MM-DD');
+    let fiveYearsAgo = moment().subtract(61, 'months').format('YYYY-MM-DD');
     return $.ajax({
-        url: `https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}?from=2015-01-01&to=${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}&apikey=${window.stockapikey}`,
+        url: `https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}?from=${fiveYearsAgo}&to=${today}&apikey=${window.stockapikey}`,
     })
 }
 
