df867800692fd3a88003cc053b5771c904bf8387
Brennan Romance <brennan.romance@gmail.com>
2020-11-19 22:39:33 -0500

    add google analytics tracking to splash page

---
+++ db/migrate/20201120033424_drop_stocks_table_again.rb
@@ -0,0 +1,5 @@
+class DropStocksTableAgain < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :stocks
+  end
+end
--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from './header'
 import ContentAndFooter from './content_and_footer';
 import SplashCover from './cover';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-3K1ZZJZ07F');
 
 class SplashPage extends React.Component {
 
