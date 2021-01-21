f986abc46710fb9aebb3beca000b5b65156b7d43
Brennan Romance <brennan.romance@gmail.com>
2021-01-21 18:59:03 -0500

    change main graph to functional component

--- frontend/components/main/graph_main.jsx
+++ frontend/components/main/graph_main.jsx
@@ -1,35 +1,25 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { LineChart, Line, CartesianGrid, YAxis, XAxis, Tooltip } from 'recharts';
 import Holidays from 'date-holidays';
 import moment from 'moment';
 import numeral from 'numeral';
 
-class GraphMain extends React.Component {
-  constructor(props) {
-    super(props);
-    this.changeTimeFrames = this.changeTimeFrames.bind(this);
-    this.handleHover = this.handleHover.bind(this);
-    this.handleMouseLeave = this.handleMouseLeave.bind(this);
-    this.customToolTip = this.customToolTip.bind(this);
-    this.filterGraphPrices = this.filterGraphPrices.bind(this);
-    this.state = {
-      equityBalance: [],
-      time: "1d",
-      premarket: ''
-    };
+function GraphMain(props) {
+  const [time, setTime ] = useState("1d");
+  const [premarket, setPremarket] = useState("")
+
+  useEffect(() => {
+    getOnlyNecessaryTimeFrames();
+  }, [])
+
+  function changeTimeFrames(newFrame) {
+    setTime(newFrame);
+    changeTimeFrameUnderline(newFrame)
   }
 
-  componentDidMount() {
-    this.getOnlyNecessaryTimeFrames();
-  }
-
-  changeTimeFrames(newFrame) {
-    this.setState({ time: newFrame });
-    this.changeTimeFrameUnderline(newFrame)
-  }
-
-  changeTimeFrameUnderline(timeFrame) {
+  function changeTimeFrameUnderline(timeFrame) {
     let lis = Array.prototype.slice.call(document.getElementsByClassName("stock-time-frame"))
     lis.forEach((li, idx) => {
       let liClassList = Array.prototype.slice.call(li.classList)
@@ -42,7 +32,7 @@ class GraphMain extends React.Component {
     })
   }
 
-  handleHover(e) {
+  function handleHover(e) {
     if (!e.activePayload) return null;
 
     let rtv = document.getElementById("current-valuation");
@@ -69,20 +59,20 @@ class GraphMain extends React.Component {
     }
   }
 
-  handleMouseLeave() {
+  function handleMouseLeave() {
     let rtv = document.getElementById("current-valuation");
     let startPrice = document.getElementById("main-starting-price");
     let diff = document.getElementById("main-diff");
     let perc = document.getElementById("main-perc");
 
     let totalEquity = 0;
-    this.props.tickers.forEach((ticker, idx) => {
-      let value = this.props.holdings[ticker].quantity * this.props.price[ticker];
+    props.tickers.forEach((ticker, idx) => {
+      let value = props.holdings[ticker].quantity * props.price[ticker];
       totalEquity += value;
     });
 
     if (!!startPrice.textContent) {
-      let currentPrice = totalEquity + this.props.buyingPower;
+      let currentPrice = totalEquity + props.buyingPower;
       let currentDiff = currentPrice - startPrice.textContent;
       let currentPerc = currentDiff / startPrice.textContent;
 
@@ -101,7 +91,7 @@ class GraphMain extends React.Component {
     }
   }
   
-  customToolTip(e) {
+  function customToolTip(e) {
     let formatted
     let dayTime
     if (!!e.label) {
@@ -110,9 +100,9 @@ class GraphMain extends React.Component {
       let makeTen = 10 - parseInt(tt[1][1]);
       dayTime = moment(e.label).add(makeTen, "minutes");
     }
-    if (this.state.time === "1d") {
+    if (time === "1d") {
       formatted = moment(dayTime).format('LT') + ' ET';
-    } else if (this.state.time === "1w") {
+    } else if (time === "1w") {
       formatted = moment(dayTime).format('LLL');
     } else {
       formatted = moment(e.label).format('MMM Do, YYYY');
@@ -122,8 +112,8 @@ class GraphMain extends React.Component {
     )
   }
 
-  getOnlyNecessaryTimeFrames() {
-    let snapshots = this.props.snapshots;
+  function getOnlyNecessaryTimeFrames() {
+    let snapshots = props.snapshots;
     if (Object.values(snapshots).length === 0) return;
 
     let timeFrames = Array.from(document.getElementsByClassName("stock-time-frame"));
@@ -162,7 +152,7 @@ class GraphMain extends React.Component {
     }
   }
 
-  getOnlyDayEndPrice(data) {
+  function getOnlyDayEndPrice(data) {
     let newData = [];
     data = data.forEach(obj => {
       if (newData.length === 0) {
@@ -177,30 +167,30 @@ class GraphMain extends React.Component {
     return newData
   }
 
-  filterGraphPrices() {
+  function filterGraphPrices() {
     let rightNow = new Date();
     let dayNum = rightNow.getDay();
     let marketOpen = moment(rightNow.toString().split(" ").slice(0, 4).join(" ") + " 09:30:00")
     if (marketOpen.isAfter(rightNow)) {
-      this.setState({ premarket: 'Markets open at 9:30am EST' })
+      setPremarket('Markets open at 9:30am EST')
     }
 
     let holidays = new Holidays('US');
     let hd = holidays.isHoliday(new Date());
-    let data = this.props.snapshots;
+    let data = props.snapshots;
     
-    if (this.state.time === "1d" && (dayNum === 6 // Saturday
+    if (time === "1d" && (dayNum === 6 // Saturday
                                     || dayNum === 0 // Sunday
                                     || hd.type === 'public' 
                                     || hd.type === 'bank'
                                     || marketOpen.isAfter(rightNow))) {
-      let mostRecentTradingDay = moment(this.props.snapshots[this.props.snapshots.length - 1].created_at)
+      let mostRecentTradingDay = moment(props.snapshots[props.snapshots.length - 1].created_at)
       data = data.filter(obj => {
           return moment(obj.created_at).isSame(mostRecentTradingDay, 'day')
       })
-    } else if (this.state.time === "1d") {
+    } else if (time === "1d") {
       data = data.filter(obj => moment(obj.created_at).isSame(rightNow, 'day'));
-    } else if (this.state.time === "1w") {
+    } else if (time === "1w") {
       data = data.filter(obj => {
         let t = obj.created_at.split("T");
         let tt = t[1].split(":");
@@ -211,128 +201,126 @@ class GraphMain extends React.Component {
         let limit = moment().subtract(1, 'weeks')
         return moment(obj.created_at).isAfter(limit) && keep;
       });
-    } else if (this.state.time === "1m") {
+    } else if (time === "1m") {
       let limit = moment().subtract(1, 'months')
       data = data.filter((obj, idx) => {
         return moment(obj.created_at).isAfter(limit);
       });
-      data = this.getOnlyDayEndPrice(data);
-    } else if (this.state.time === "3m") {
+      data = getOnlyDayEndPrice(data);
+    } else if (time === "3m") {
       let limit = moment().subtract(3, 'months')
       data = data.filter(obj => {
           return moment(obj.created_at).isAfter(limit);
       });
-      data = this.getOnlyDayEndPrice(data);
-    } else if (this.state.time === "1y") {
+      data = getOnlyDayEndPrice(data);
+    } else if (time === "1y") {
       let limit = moment().subtract(1, 'years')
       data = data.filter(obj => {
           return moment(obj.created_at).isAfter(limit);
       });
-      data = this.getOnlyDayEndPrice(data);
-    } else if (this.state.time === "5y") {
+      data = getOnlyDayEndPrice(data);
+    } else if (time === "5y") {
       let limit = moment().subtract(5, 'years')
       data = data.filter((obj, idx) => {
           return moment(obj.created_at).isAfter(limit)
       });
-      data = this.getOnlyDayEndPrice(data);
+      data = getOnlyDayEndPrice(data);
     }
     return data
   }
-
-  render() {  
-    if (Object.keys(this.props.graphPrices).length !== Object.keys(this.props.price).length
-      || this.props.buyingPower.length === 0) {
-      return null
-    }
-
-    let data = this.filterGraphPrices();
-
-    let totalEquity = 0;
-    this.props.tickers.forEach((ticker, idx) => {
-      if(this.props.holdings[ticker].quantity !== 0) {
-        let value = this.props.holdings[ticker].quantity * this.props.price[ticker];
-        totalEquity = totalEquity + value;
-      }
-    });
-
-    let assets = totalEquity + this.props.buyingPower;
-    let difference;
-    let percentage;
-    let start;
-    let color;
-    let docBody = document.body;
-
-    if (data[0]) {
-      start = data[0].valuation;
-      difference = assets - start;
-      percentage =  difference / start;
-      if (difference > 0) {
-        difference = numeral(difference).format('$0,0.00');
-        percentage = numeral(percentage).format('0.00%');
-        difference = `(+${difference.toString()})`;
-        percentage = `+${percentage.toString()}`;
-      } else {
-        percentage = `(${numeral(percentage).format('0.00%')})`
-        difference = numeral(difference).format('$0,0.00')
-      }
-
-      if (data[0] !== undefined && data[0].valuation > data.slice(-1)[0].valuation) {
-        docBody.setAttribute("data-trend", "down");
-        color = '#f45531';
-      } else {
-        docBody.setAttribute("data-trend", "up");
-        color = '#21ce99';
-      }
-    }
-
-    let toolTipOffSet;
-    this.state.time === '1w' ? toolTipOffSet = -70 : toolTipOffSet = -50;
-
-    const renderLineChart = (
-      <LineChart
-        width={800}
-        height={300}
-        data={data}
-        onMouseMove={this.handleHover}
-        onMouseLeave={this.handleMouseLeave}>
-        <Line type="linear" dataKey="valuation" stroke={color} dot={false} strokeWidth={2}/>
-        <YAxis domain={['dataMin', 'dataMax']} axisLine={false} hide={true}/>
-        <XAxis dataKey='created_at' hide={true} />
-        <Tooltip
-          position={{ y: 0 }}
-          offset={toolTipOffSet}
-          isAnimationActive={false}
-          content={this.customToolTip}
-          wrapperStyle={{ top: -15 }}
-        />
-      </LineChart>
-    );
-
-    return (
-      <div>
-        <h2 id="current-valuation" className="main-page-total-assets">
-          {numeral(this.props.buyingPower + totalEquity).format('$0,0.00')}
-        </h2>
-        <div className="main-percentage-and-difference">
-          <p className="main-page-difference" id="main-diff">{difference}</p>
-          <p className="main-page-percentage" id="main-perc">{percentage}</p>
-          <li className="hide" id="main-starting-price">{start}</li>
-        </div>
-        {renderLineChart}
-        <ul className="stock-time-frames">
-          <h2 onClick={() => this.changeTimeFrames("1d")} className="stock-time-frame 1d underlined">1D</h2>
-          <h2 onClick={() => this.changeTimeFrames("1w")} className="stock-time-frame 1w">1W</h2>
-          <h2 onClick={() => this.changeTimeFrames("1m")} className="stock-time-frame 1m">1M</h2>
-          <h2 onClick={() => this.changeTimeFrames("3m")} className="stock-time-frame 3m">3M</h2>
-          <h2 onClick={() => this.changeTimeFrames("1y")} className="stock-time-frame 1y">1Y</h2>
-          <h2 onClick={() => this.changeTimeFrames("5y")} className="stock-time-frame 5y">All</h2>
-        </ul>
-        <p className="markets-closed-message">
-          {this.state.premarket}
-        </p>
-      </div>
-    )
+ 
+  if (Object.keys(props.graphPrices).length !== Object.keys(props.price).length
+    || props.buyingPower.length === 0) {
+    return null
   }
+
+  let data = filterGraphPrices();
+
+  let totalEquity = 0;
+  props.tickers.forEach((ticker, idx) => {
+    if(props.holdings[ticker].quantity !== 0) {
+      let value = props.holdings[ticker].quantity * props.price[ticker];
+      totalEquity = totalEquity + value;
+    }
+  });
+
+  let assets = totalEquity + props.buyingPower;
+  let difference;
+  let percentage;
+  let start;
+  let color;
+  let docBody = document.body;
+
+  if (data[0]) {
+    start = data[0].valuation;
+    difference = assets - start;
+    percentage =  difference / start;
+    if (difference > 0) {
+      difference = numeral(difference).format('$0,0.00');
+      percentage = numeral(percentage).format('0.00%');
+      difference = `(+${difference.toString()})`;
+      percentage = `+${percentage.toString()}`;
+    } else {
+      percentage = `(${numeral(percentage).format('0.00%')})`
+      difference = numeral(difference).format('$0,0.00')
+    }
+
+    if (data[0] !== undefined && data[0].valuation > data.slice(-1)[0].valuation) {
+      docBody.setAttribute("data-trend", "down");
+      color = '#f45531';
+    } else {
+      docBody.setAttribute("data-trend", "up");
+      color = '#21ce99';
+    }
+  }
+
+  let toolTipOffSet;
+  time === '1w' ? toolTipOffSet = -70 : toolTipOffSet = -50;
+
+  const renderLineChart = (
+    <LineChart
+      width={800}
+      height={300}
+      data={data}
+      onMouseMove={handleHover}
+      onMouseLeave={handleMouseLeave}>
+      <Line type="linear" dataKey="valuation" stroke={color} dot={false} strokeWidth={2}/>
+      <YAxis domain={['dataMin', 'dataMax']} axisLine={false} hide={true}/>
+      <XAxis dataKey='created_at' hide={true} />
+      <Tooltip
+        position={{ y: 0 }}
+        offset={toolTipOffSet}
+        isAnimationActive={false}
+        content={customToolTip}
+        wrapperStyle={{ top: -15 }}
+      />
+    </LineChart>
+  );
+
+  return (
+    <div>
+      <h2 id="current-valuation" className="main-page-total-assets">
+        {numeral(props.buyingPower + totalEquity).format('$0,0.00')}
+      </h2>
+      <div className="main-percentage-and-difference">
+        <p className="main-page-difference" id="main-diff">{difference}</p>
+        <p className="main-page-percentage" id="main-perc">{percentage}</p>
+        <li className="hide" id="main-starting-price">{start}</li>
+      </div>
+      {renderLineChart}
+      <ul className="stock-time-frames">
+        <h2 onClick={() => changeTimeFrames("1d")} className="stock-time-frame 1d underlined">1D</h2>
+        <h2 onClick={() => changeTimeFrames("1w")} className="stock-time-frame 1w">1W</h2>
+        <h2 onClick={() => changeTimeFrames("1m")} className="stock-time-frame 1m">1M</h2>
+        <h2 onClick={() => changeTimeFrames("3m")} className="stock-time-frame 3m">3M</h2>
+        <h2 onClick={() => changeTimeFrames("1y")} className="stock-time-frame 1y">1Y</h2>
+        <h2 onClick={() => changeTimeFrames("5y")} className="stock-time-frame 5y">All</h2>
+      </ul>
+      <p className="markets-closed-message">
+        {premarket}
+      </p>
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
