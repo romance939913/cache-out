f1f0a9ba00f89fdfc105e3c2c647820178d408b6
Brennan <brennan.romance@gmail.com>
2021-06-21 14:43:23 -0400

    add todos for practice

--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './header'
 import ContentAndFooter from './content_and_footer';
 import SplashCover from './cover';
+import TodosComponent from './todos';
 import { connect } from 'react-redux';
 
 const SplashPage = () => (
   <div>
     <Header />
+    {/* <TodosComponent /> */}
     <SplashCover />
     <ContentAndFooter />
   </div>
